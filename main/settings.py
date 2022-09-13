fbb3e39e00dfd8514732df7ba338d2ad77f616da
Ravi Kiran <83539741+ravikiran009@users.noreply.github.com>
2022-09-13 07:55:22 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(6ls#!#(z^ia-gvqa#%tu58sr3^#w#tp@zp)5nr=ox!ev!r#7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog9website.herokuapp.com']
 
 
 # Application definition
