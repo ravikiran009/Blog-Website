0a381155277c10e36239c11060de5fc36b75dd78
Ravi Kiran <83539741+ravikiran009@users.noreply.github.com>
2022-09-13 12:45:11 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIR = [ os.path.join(BASE_DIR,'blog/static'),] 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
