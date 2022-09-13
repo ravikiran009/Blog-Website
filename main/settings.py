c2de216ee24ba4f6402988ac53723290ca6ba65d
Ravi Kiran <83539741+ravikiran009@users.noreply.github.com>
2022-09-13 14:48:28 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIR = [os.path.join(BASE_DIR,'blog/static/')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-(6ls#!#(z^ia-gvqa#%tu58sr3^#w#tp@zp)5nr=ox!ev!r#7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog9website.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
